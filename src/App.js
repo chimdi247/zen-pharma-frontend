58bcddb0935f61d64af47e60d41eeb7855e25d50
chimdi247 <Chimdi247@gmail.com>
2026-05-11 21:13:24 +0300

    Remove unused PropTypes import from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
